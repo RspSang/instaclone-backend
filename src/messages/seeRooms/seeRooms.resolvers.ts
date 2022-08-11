c088e94144c8b178399308e6b0eb6af48d36f7d8
DeepDive <71082215+RspSang@users.noreply.github.com>
2022-08-11 13:12:52 +0900

    See Room & Send Message

---
+++ src/messages/seeRooms/seeRooms.resolvers.ts
@@ -0,0 +1,11 @@
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+  Query: {
+    seeRooms: protectedResolver(async (_, __, { client, loggedInUser }) =>
+      client.room.findMany({
+        where: { users: { some: { id: loggedInUser.id } } },
+      })
+    ),
+  },
+};
