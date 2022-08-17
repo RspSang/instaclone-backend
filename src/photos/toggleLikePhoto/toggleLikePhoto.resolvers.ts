766d579abaa4da57f1654e1f2048cb93efd22b0e
DeepDive <71082215+RspSang@users.noreply.github.com>
2022-08-17 17:19:43 +0900

    Fix toggleLike bug

--- src/photos/toggleLikePhoto/toggleLikePhoto.resolvers.ts
+++ src/photos/toggleLikePhoto/toggleLikePhoto.resolvers.ts
@@ -8,44 +8,48 @@ export default {
   Mutation: {
     toggleLike: protectedResolver(
       async (_, { id }: ToggleLikeArgs, { client, loggedInUser }) => {
-        const photo = await client.photo.findUnique({ where: { id } });
-        if (!photo) {
-          return {
-            ok: false,
-            error: "사진을 찾을수 없습니다.",
+        try {
+          const photo = await client.photo.findUnique({ where: { id } });
+          if (!photo) {
+            return {
+              ok: false,
+              error: "사진을 찾을수 없습니다.",
+            };
+          }
+          const likeWhere = {
+            photoId_userId: {
+              userId: loggedInUser.id,
+              photoId: id,
+            },
           };
-        }
-        const likeWhere = {
-          photoId_userId: {
-            userId: loggedInUser.id,
-            photoId: id,
-          },
-        };
-        const like = await client.like.findUnique({
-          where: likeWhere,
-        });
-        if (like) {
-          await client.like.delete({
+          const like = await client.like.findUnique({
             where: likeWhere,
           });
-        } else {
-          await client.like.create({
-            data: {
-              user: {
-                connect: {
-                  id: loggedInUser.id,
+          if (like) {
+            await client.like.delete({
+              where: likeWhere,
+            });
+          } else {
+            await client.like.create({
+              data: {
+                user: {
+                  connect: {
+                    id: loggedInUser.id,
+                  },
+                },
+                photo: {
+                  connect: {
+                    id: photo.id,
+                  },
                 },
               },
-              photo: {
-                connect: {
-                  id: photo.id,
-                },
-              },
-            },
-          });
-          return {
-            ok: true,
-          };
+            });
+            return {
+              ok: true,
+            };
+          }
+        } catch (error) {
+          console.log(error);
         }
       }
     ),
