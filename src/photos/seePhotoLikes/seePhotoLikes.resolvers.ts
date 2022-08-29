9321e3c4ce2432b75ba70dc2daf96f6a6861e2bf
DeepDive <71082215+RspSang@users.noreply.github.com>
2022-08-29 16:28:05 +0900

    Fix seePhotoLikes bugs

--- src/photos/seePhotoLikes/seePhotoLikes.resolvers.ts
+++ src/photos/seePhotoLikes/seePhotoLikes.resolvers.ts
@@ -7,7 +7,7 @@ export default {
     seePhotoLikes: async (_, { id }: SeePhotoLikesArgs, { client }) => {
       const likes = await client.like.findMany({
         where: { photoId: id },
-        select: { user: { select: { user: true } } },
+        select: { user: true },
       });
       return likes.map((like) => like.user);
     },
